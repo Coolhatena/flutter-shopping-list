9a8038fd79630fd26ef39d620966089e7d9fdae4
Eduardo Rivera Geffroy <ingsc.geffroy@gmail.com>
2024-11-14 15:14:58 -0800

    Make list items dismissable & add fallback screen when theres no list items

--- lib/screens/grocery_list_screen.dart
+++ lib/screens/grocery_list_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:shopping_list/data/dummy_data.dart';
+import 'package:shopping_list/models/grocery_item.dart';
 import 'package:shopping_list/screens/new_item_screen.dart';
 import 'package:shopping_list/widgets/grocery_list_item.dart';
 
@@ -13,12 +14,43 @@ class GroceryListScreen extends StatefulWidget {
 }
 
 class _GroceryListScreenState extends State<GroceryListScreen> {
-  void _addItem() {
-    Navigator.of(context).push(
+  final List<GroceryItem> _groceryItems = [];
+
+  void _addItem() async {
+    final newItem = await Navigator.of(context).push<GroceryItem>(
       MaterialPageRoute(
         builder: (ctx) => const NewItemScreen(),
       ),
     );
+
+    if (newItem == null) {
+      return;
+    }
+
+    setState(() {
+      _groceryItems.add(newItem);
+    });
+  }
+
+  void _removeItem(GroceryItem item) {
+    final itemIndex = _groceryItems.indexOf(item);
+
+    setState(() {
+      _groceryItems.remove(item);
+    });
+    ScaffoldMessenger.of(context).clearSnackBars();
+    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+      duration: const Duration(seconds: 3),
+      content: const Text('Grocery item deleletd'),
+      action: SnackBarAction(
+        label: 'Undo',
+        onPressed: () {
+          setState(() {
+            _groceryItems.insert(itemIndex, item);
+          });
+        },
+      ),
+    ));
   }
 
   @override
@@ -28,14 +60,28 @@ class _GroceryListScreenState extends State<GroceryListScreen> {
         title: const Text('Your Groceries'),
         actions: [IconButton(onPressed: _addItem, icon: const Icon(Icons.add))],
       ),
-      body: ListView.builder(
-        itemCount: groceryItems.length,
-        itemBuilder: (ctx, index) => GroceryListItem(
-          color: groceryItems[index].category.color,
-          name: groceryItems[index].name,
-          quantity: groceryItems[index].quantity,
-        ),
-      ),
+      // TODO: Separate the body code in individual widgets to improve readability
+      body: _groceryItems.isEmpty
+          ? const Center(
+              child: Text(
+                'No groceries added. Start adding some!',
+                style: TextStyle(fontSize: 16),
+              ),
+            )
+          : ListView.builder(
+              itemCount: _groceryItems.length,
+              itemBuilder: (ctx, index) => Dismissible(
+                key: ValueKey(_groceryItems[index].id),
+                onDismissed: (direction) {
+                  _removeItem(_groceryItems[index]);
+                },
+                child: GroceryListItem(
+                  color: _groceryItems[index].category.color,
+                  name: _groceryItems[index].name,
+                  quantity: _groceryItems[index].quantity,
+                ),
+              ),
+            ),
     );
   }
 }
