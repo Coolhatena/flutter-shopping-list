a95a5b9fd30ac69a8ff9278875f3e1942e70d75e
Eduardo Rivera Geffroy <ingsc.geffroy@gmail.com>
2024-11-19 10:59:59 -0800

    Add new items to the list without doing excesive http request

--- lib/screens/grocery_list_screen.dart
+++ lib/screens/grocery_list_screen.dart
@@ -3,7 +3,6 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:shopping_list/data/categories.dart';
-import 'package:shopping_list/models/category.dart';
 import 'package:shopping_list/models/grocery_item.dart';
 import 'package:shopping_list/screens/new_item_screen.dart';
 import 'package:shopping_list/widgets/grocery_list_item.dart';
@@ -35,7 +34,7 @@ class _GroceryListScreenState extends State<GroceryListScreen> {
     // print(response.body);
 
     final Map<String, dynamic> listData = json.decode(response.body);
-    final List<GroceryItem> _loadedItems = [];
+    final List<GroceryItem> loadedItems = [];
 
     for (final item in listData.entries) {
       final category = categories.entries
@@ -43,7 +42,7 @@ class _GroceryListScreenState extends State<GroceryListScreen> {
               categoryItem.value.name == item.value['category'])
           .value;
 
-      _loadedItems.add(GroceryItem(
+      loadedItems.add(GroceryItem(
         id: item.key,
         name: item.value['name'],
         quantity: item.value['quantity'],
@@ -52,18 +51,24 @@ class _GroceryListScreenState extends State<GroceryListScreen> {
     }
 
     setState(() {
-      _groceryItems = _loadedItems;
+      _groceryItems = loadedItems;
     });
   }
 
   void _addItem() async {
-    await Navigator.of(context).push<GroceryItem>(
+    final newItem = await Navigator.of(context).push<GroceryItem>(
       MaterialPageRoute(
         builder: (ctx) => const NewItemScreen(),
       ),
     );
 
-    _loadItems();
+    if (newItem == null) {
+      return;
+    }
+
+    setState(() {
+      _groceryItems.add(newItem);
+    });
   }
 
   void _removeItem(GroceryItem item) {
